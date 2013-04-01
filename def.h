befcdb3a6507af3e222cbfa75bb042efcb7d3e89
Helena_Wu <helena.wu87@gmail.com>
2013-04-01 20:43:24 +0200

    ajoute def.h, main.c, regles

---
+++ def.h
@@ -0,0 +1,21 @@
+#ifndef STRUCT_CERCLE
+#define STRUCT_CERCLE 
+
+typedef struct cercle{
+  int vide;
+  char couleur;
+  
+}cercle;
+
+#endif
+
+
+#ifndef STRUCT_REGLE
+#define STRUCT_REGLE 
+
+typedef struct regle{
+  char cas; //
+
+}regle;
+
+#endif
---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "def.h"
+
+
+int main(int argc, char ** argv){
+  cercle tab[9][9] = malloc(sizeof(cercle) * 81);
+  
+  
+}
+
+
+
+void initTableau(cercle[9][9] tab){
+  
+}
+
